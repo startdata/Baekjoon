ad56c3467f4bd6d77b9ee1949b0186088e965f0e
startas <83121954+startdata@users.noreply.github.com>
2023-01-14 22:14:46 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -0,0 +1,25 @@
+function solution(keyinput, board) {
+    let x = 0;
+    let y = 0;
+    
+    const range = 
+          [ Math.abs( Math.floor(board[0] / 2) ), 
+           Math.abs( Math.floor(board[1] / 2) ) ];
+    
+    for(let k of keyinput){
+        switch(k){
+            case "left": x--; break;
+            case "right": x++; break;
+            case "down": y--; break;
+            case "up": y++; break;
+        }
+        if(Math.abs(x)>range[0]){
+            x = x > 0 ? range[0] : range[0] * -1;
+         }
+        if(Math.abs(y)>range[1]){
+            y = y > 0 ? range[1] : range[1] * -1;
+        }
+        
+    }
+    return [x,y];
+}
