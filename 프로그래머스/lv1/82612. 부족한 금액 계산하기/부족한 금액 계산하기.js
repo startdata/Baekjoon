952c72c9b0bfe13b6c2de5c56549a8e88abdc76f
startas <83121954+startdata@users.noreply.github.com>
2023-01-27 00:11:11 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,13 @@
+function solution(price, money, count) {
+    let a = 0;
+    for(let i = 1; i<=count; i++){
+        a += price*i
+    }
+    let answer = Math.abs(money - a)
+    if(money > a){
+        return 0
+    } else {
+        return answer
+    }
+
+}
