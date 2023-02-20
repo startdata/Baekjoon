7d9c60732eff9893f1238152c6a74c51a13efa33
startas <83121954+startdata@users.noreply.github.com>
2023-02-21 00:56:51 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    const dp = Array.from({length:n}).fill(0);
+    dp[0] = 1, dp[1] = 1; 
+  	for (let i=2; i<=n; i++) {
+        dp[i] = (dp[i-2] + dp[i-1])%1234567;
+    }
+    return dp[n];
+}
