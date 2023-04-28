4ac1daff60b665ca135b516507b25bbff09b0b4c
startas <83121954+startdata@users.noreply.github.com>
2023-04-29 02:40:16 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    if(a%2 ===1 && b%2 ===1){
+        return Math.pow(a,2)+Math.pow(b,2)
+    } else if(a%2 === 1 && b%2 ===0){
+        return 2*(a+b)
+    } else if(a%2 === 0 && b%2 ===1){
+        return 2*(a+b)
+    } else {
+        return Math.abs(a-b)
+    }
+}
