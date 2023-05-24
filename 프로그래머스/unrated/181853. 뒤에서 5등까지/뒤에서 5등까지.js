c91811450cc1ed5a97b512eb8ee36780fb0b223f
startas <83121954+startdata@users.noreply.github.com>
2023-05-25 00:35:52 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.js
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.js
@@ -1,9 +1,6 @@
 function solution(num_list) {
     let answer = [];
-    const newArr = num_list.map(list => list);
+    const newArr = num_list.map(list => list)
     const sortArr = newArr.sort((a,b) => a-b);
-    for(let i =0; i<5;i++){
-        answer.push(sortArr[i]);
-    }
-    return answer;
+    return sortArr.filter((item,index) => index <5);
 }
