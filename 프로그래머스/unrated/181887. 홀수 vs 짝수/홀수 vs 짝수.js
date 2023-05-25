fca349884d60bb220028caedf91cf75f41194b65
startas <83121954+startdata@users.noreply.github.com>
2023-05-26 04:21:45 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    let odd = 0;
+    let even = 0;
+    for(let i =0; i<num_list.length;i++){
+        if(i % 2 === 0) {
+            even += num_list[i];
+        } else {
+            odd += num_list[i];
+        }    
+    }
+    
+    return odd > even ? odd : even;
+}
