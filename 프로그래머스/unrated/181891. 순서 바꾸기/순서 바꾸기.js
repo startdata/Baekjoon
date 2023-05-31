ed62b72aec0fc2be65867b2bf4edebe40e916932
startas <83121954+startdata@users.noreply.github.com>
2023-06-01 02:07:12 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.js
@@ -0,0 +1,10 @@
+function solution(num_list, n) {
+    var answer = [];
+    for(let i =n; i<num_list.length;i++){
+        answer.push(num_list[i]);
+    }
+    for(let i=0; i<n;i++){
+        answer.push(num_list[i]);
+    }
+    return answer;
+}
