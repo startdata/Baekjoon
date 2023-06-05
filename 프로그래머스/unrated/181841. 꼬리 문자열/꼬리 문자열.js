d15af693cb2af5532590722e2dd955405f4f83b8
startas <83121954+startdata@users.noreply.github.com>
2023-06-06 01:19:02 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
@@ -0,0 +1,10 @@
+function solution(str_list, ex) {
+    let arr =[];
+    for(let i=0;i<str_list.length;i++){
+        if(!str_list[i].includes(ex)){
+            arr.push(str_list[i])
+        }
+    }
+    return arr.join('')
+    
+}
