5450a4fdd14922963027b9199e1e19d23a9cba96
startas <83121954+startdata@users.noreply.github.com>
2023-06-07 00:30:17 +0900

    [unrated] Title: A 강조하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -1,3 +1,3 @@
 function solution(myString) {
-   return [...myString.replaceAll('a', 'A')].map(a => (a.charCodeAt() > 65 && a.charCodeAt() < 97) ? a.toLowerCase() : a).join("")
+   return myString.toLowerCase().replaceAll('a','A')
 }
