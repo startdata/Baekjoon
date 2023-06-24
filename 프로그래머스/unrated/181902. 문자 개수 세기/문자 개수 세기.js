5fdf404ceab5a57969d35ad2aa5b84f6b60b6085
startas <83121954+startdata@users.noreply.github.com>
2023-06-25 02:01:31 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,16 @@
+function solution(my_string) {
+    const arr = Array(52).fill(0)
+    return [...my_string].reduce((acc, cur) => {
+        let curIdx
+        const curAlpha= cur.charCodeAt()
+        if(curAlpha >= 97) {
+         curIdx = curAlpha - 71   
+        } else {
+         curIdx = curAlpha-65   
+        }
+         
+        acc[curIdx] = acc[curIdx] ?? 0
+        acc[curIdx]++
+        return acc
+    }, arr)
+}
