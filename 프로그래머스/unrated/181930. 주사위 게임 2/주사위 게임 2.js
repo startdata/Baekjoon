2655f0c4fe2e8aa8a2a82b5b94b8871772054e11
startas <83121954+startdata@users.noreply.github.com>
2023-06-26 00:45:52 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,11 +1,11 @@
 function solution(a, b, c) {
-   
-        if(a === b && b === c && c === a){
-           return (a+b+c)*(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2))*(Math.pow(a,3)+Math.pow(b,3)+Math.pow(c,3))
-        }
-        else if(a !==b && b!==c && c!==a){
-            return a+b+c
-        } else if(a===b && a!==c || b===c && a!==b || c===a && a!==b){
-            return (a+b+c)*(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2))            
-        }    
+   const set = new Set([a,b,c]).size
+       if(set ===1 ){
+            return (a+b+c)*(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2))*(Math.pow(a,3)+Math.pow(b,3)+Math.pow(c,3))     
+       } else if(set ===2){
+           return (a+b+c)*(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2))   
+       } else if(set ===3) {
+           return a+b+c     
+       }
+            
 }
