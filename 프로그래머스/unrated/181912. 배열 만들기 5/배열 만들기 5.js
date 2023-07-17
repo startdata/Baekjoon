babfaec99630a60c1ecbe4fec88126234a6c2d4b
startas <83121954+startdata@users.noreply.github.com>
2023-07-18 01:02:47 +0900

    [unrated] Title: 배열 만들기 5, Time: 1.47 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
@@ -0,0 +1,9 @@
+function solution(intStrs, k, s, l) {
+   const result = []
+    intStrs.forEach(a => {
+        const sliceStr = a.slice(s, s+l)
+        const num = Number(sliceStr)
+        if(num > k) result.push(num)
+    })
+    return result
+}
